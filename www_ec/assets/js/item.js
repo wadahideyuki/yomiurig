b85f2898c65b8e4e576f178198d4feb3db85c084
smasuzawa <koubousei@gmail.com>
2018-06-07 11:50:06 +0900

    EC 修正

--- www_ec/assets/js/item.js
+++ www_ec/assets/js/item.js
@@ -1,3 +1,4 @@
+/*-------- スライド --------*/
 $(function() {
 	$('.slick-box').slick({
 		infinite: true,
@@ -5,13 +6,24 @@ $(function() {
 		speed:100
 	}); 
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+	pagination: ".swiper-pagination",
+	paginationClickable: true
+});
+/*-------- /スライド --------*/
+
+
 $(document).ready(function() {
   //サムネイル差し替え
   $(".item-thumbs li").click(function(){
     $(".item-thumbs li").removeClass();
     $(this).addClass("selected");
     var imgSrc = $("img",this).attr("src");
-    $(".item-photo img").attr("src",imgSrc);
+    $(".item-photo .pc-photo img").attr("src",imgSrc);
   });
   
   //SP商品詳細
